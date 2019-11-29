a788b20a71463ba2bf87c2c21a3fc281f1ff42fc
vidcharensuk <saravidsuchaad@gmail.com>
2019-11-29 12:16:18 +0800

    added smart kettle code

---
+++ smart kettle/kettle.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract ledswitch {
+    uint temp;//temperature value
+    uint limit;//upper limit value
+    uint power;//set power value
+
+    event overlimit(uint temp);
+
+    //Store current temperature of kettle
+    function storeTemp(uint t) public{
+        temp = t;
+        if(temp >= limit){
+            emit overlimit(temp);
+        }
+    }
+    //Set upper limit to heat water
+    function setLimit(uint k) public{
+        limit = k;
+    }
+
+    //Set power switch
+    function setPower(uint p){
+        power = p;
+    }
+
+    //Obtain current power switch command
+    function getPower() public view returns(uint){
+        return power;
+    }
---
+++ smart kettle/kettleuid.py
@@ -0,0 +1,33 @@
+#author = "Vid Charensuk"
+#license = "GPLv3"
+#version = "0.1"
+#maintainer = "Vid Charensuk"
+#email = "saravidsuchaad@gmail.com"
+#status = "Development"
+
+import json
+from web3 import Web3
+import time
+import datetime
+
+node_url = "http://127.0.0.1:7545"
+web3 = Web3(Web3.HTTPProvider(node_url))
+
+web3.eth.defaultAccount = web3.eth.accounts[0]
+
+abi = json.loads('[{"constant":false,"inputs":[{"internalType":"bool","name":"value","type":"bool"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}]')
+
+#contract address
+address = web3.toChecksumAddress("0xde4936DD510115B8B583C039858ABce8f76Dc531")
+
+contract = web3.eth.contract(address=address, abi=abi)  
+
+print("Current Switch Status is: ", contract.functions.getSwitch().call)
+
+while True:
+    print("1: Turn On, 2: Turn Off")
+    on = input()
+    tx_hash = contract.functions.setSwitch(on).transact
+    web3.eth.waitForTransactionReceipt(tx_hash)
+    print("Done")
+    print("Currently: ", on)
