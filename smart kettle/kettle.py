a4f1a2ab0520824a02cca7a4e6e341b0805ae2b4
vidcharensuk <saravidsuchaad@gmail.com>
2019-12-04 09:15:20 +0800

    changed abi in py to be compatible with sol

--- smart kettle/kettle.py
+++ smart kettle/kettle.py
@@ -14,17 +14,16 @@ import datetime
 tempSensor = 0
 LED = 0
 
+#Initialization connection to ethereum node
 node_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(node_url))
-
 web3.eth.defaultAccount = web3.eth.accounts[0]
-
+#ABI for solidity contract code - change when sol is editted
 abi = json.loads('[{"constant":false,"inputs":[{"internalType":"uint256","name":"k","type":"uint256"}],"name":"setLimit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPower","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"t","type":"uint256"}],"name":"storeTemp","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"p","type":"uint256"}],"name":"setPower","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"temp","type":"uint256"}],"name":"overlimit","type":"event"}]')
-
 #contract address
 address = web3.toChecksumAddress("0xde4936DD510115B8B583C039858ABce8f76Dc531")
+contract = web3.eth.contract(address=address, abi=abi)
 
-contract = web3.eth.contract(address=address, abi=abi)  
 
 #Simulate Temperature from sensor
 def getTemp():
--- switch/switchuid.py
+++ switch/switchuid.py
@@ -10,17 +10,15 @@ from web3 import Web3
 import time
 import datetime
 
+#Initialization connection to ethereum node
 node_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(node_url))
-
 web3.eth.defaultAccount = web3.eth.accounts[0]
-
-abi = json.loads('[{"constant":false,"inputs":[{"internalType":"bool","name":"value","type":"bool"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}]')
-
+#ABI for solidity contract code - change when sol is editted
+abi = json.loads('[{"constant":true,"inputs":[],"name":"getSwitch","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"bool","name":"value","type":"bool"}],"name":"setSwitch","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 #contract address
 address = web3.toChecksumAddress("0xde4936DD510115B8B583C039858ABce8f76Dc531")
-
-contract = web3.eth.contract(address=address, abi=abi)  
+contract = web3.eth.contract(address=address, abi=abi)
 
 print("Current Switch Status is: ", contract.functions.getSwitch().call)
 
