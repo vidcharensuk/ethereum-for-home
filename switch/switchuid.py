0cb21036e807f829951857f950e058d7e88a7a94
Vid Charensuk <saravidsuchaad@gmail.com>
2019-12-05 14:30:29 +0800

    fix bugs for transact and if statement

--- switch/switchuid.py
+++ switch/switchuid.py
@@ -17,19 +17,19 @@ web3.eth.defaultAccount = web3.eth.accounts[0]
 #ABI for solidity contract code - change when sol is editted
 abi = json.loads('[{"constant":true,"inputs":[],"name":"getSwitch","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"bool","name":"value","type":"bool"}],"name":"setSwitch","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 #contract address
-address = web3.toChecksumAddress("0xde4936DD510115B8B583C039858ABce8f76Dc531")
+address = web3.toChecksumAddress("0xAfdBf7e24fc622Bf6DBA17eaf836CE6d6179a5D1")
 contract = web3.eth.contract(address=address, abi=abi)
 
-print("Current Switch Status is: ", contract.functions.getSwitch().call)
+print("Current Switch Status is: ", contract.functions.getSwitch().call())
 
 while True:
     print("1: Turn On, 2: Turn Off")
     on = input()
-    if (on == 1):
+    if on == '1':
         switch = True
     else:
         switch = False
-    tx_hash = contract.functions.setSwitch(switch).transact
+    tx_hash = contract.functions.setSwitch(switch).transact()
     web3.eth.waitForTransactionReceipt(tx_hash)
     print("Done")
     print("Currently: ", switch)
