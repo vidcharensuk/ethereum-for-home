421253d2212d2c44826bbe54d9f712c5afa19241
vid <vid@vids-iMac.local>
2019-12-05 16:29:43 +0800

    not important: changed contact addr

--- switch/switchuid.py
+++ switch/switchuid.py
@@ -11,13 +11,13 @@ import time
 import datetime
 
 #Initialization connection to ethereum node
-node_url = "http://127.0.0.1:7545"
+node_url = "http://127.0.0.1:8545"
 web3 = Web3(Web3.HTTPProvider(node_url))
 web3.eth.defaultAccount = web3.eth.accounts[0]
 #ABI for solidity contract code - change when sol is editted
 abi = json.loads('[{"constant":true,"inputs":[],"name":"getSwitch","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"bool","name":"value","type":"bool"}],"name":"setSwitch","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 #contract address
-address = web3.toChecksumAddress("0xAfdBf7e24fc622Bf6DBA17eaf836CE6d6179a5D1")
+address = web3.toChecksumAddress("0x3C8Ec2A37CB4fFC7aC2E48a240Ca71A4220e93d9")
 contract = web3.eth.contract(address=address, abi=abi)
 
 print("Current Switch Status is: ", contract.functions.getSwitch().call())
