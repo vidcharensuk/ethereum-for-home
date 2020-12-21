ccfb4e8bd3bb3d2d635906064091b643a3ef8adb
Vid Charensuk <saravidsuchaad@gmail.com>
2020-12-22 00:06:50 +0800

    add poa middleware and change contract addr

--- switch/deploy_contracts.py
+++ switch/deploy_contracts.py
@@ -18,6 +18,7 @@ web3.eth.defaultAccount = web3.eth.accounts[0]
 #ABI for solidity contract code - change when sol is editted
 abi = json.loads('[{"inputs":[],"name":"getSwitch","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bool","name":"value","type":"bool"}],"name":"setSwitch","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
 bytecode = "608060405234801561001057600080fd5b5060ea8061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c80636fb7575c14603757806397cefc4c146055575b600080fd5b603d6082565b60405180821515815260200191505060405180910390f35b608060048036036020811015606957600080fd5b810190808035151590602001909291905050506098565b005b60008060009054906101000a900460ff16905090565b806000806101000a81548160ff0219169083151502179055505056fea26469706673582212209e7452078991d0d808bbdfcb042b2067a1a9c2fbd5b8caa5065a3675849201db64736f6c63430007060033"
+web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 Switch = web3.eth.contract(abi=abi, bytecode=bytecode)
 
